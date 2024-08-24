60c5660e1e90634aa533da7c9cf7d5232ba6ae91
Jelly-man <kent.yates4367@gmail.com>
2024-08-24 13:07:12 +0100

    CV and profile picture added to Assets folder. Top section created adn styled with links to socials, CV download and contact me buttons.

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
+
 function toggleMenu() {
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+/**
+ * Toggles the open class on the menu-links and hamburger-icon elements, 
+ * which controls the visibility of the mobile dropdown menu.
+ * this is using the toggle method and vannilla js instead of a library like bootstrap or tailwind.
+ */
